057ee6a06ec589472868e6b26fb4376b45065057
Granja45 <140736732+Granja45@users.noreply.github.com>
2024-01-11 17:52:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ function randomizePosition() {
     var maxWidth = window.innerWidth - button.clientWidth;
     var maxHeight = window.innerHeight - button.clientHeight;
 
+    // Dimensions of text box and "Yes" button
+    var textBoxWidth = document.querySelector('.container').clientWidth;
+    var yesButtonWidth = document.getElementById('yesBtn').clientWidth;
+
+    // Adjusting the available width considering text box and "Yes" button
+    maxWidth = maxWidth - textBoxWidth - yesButtonWidth;
+
     var randomX = Math.floor(Math.random() * maxWidth);
     var randomY = Math.floor(Math.random() * maxHeight);
 
     button.style.position = 'absolute';
     button.style.left = randomX + 'px';
     button.style.top = randomY + 'px';
+}
+
+function redirectToLove() {
+    window.location.href = 'love.html';
 }
