635de6d42bc96f38f678fd32f9f22449b165b438
Granja45 <140736732+Granja45@users.noreply.github.com>
2024-01-11 19:25:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,16 +13,17 @@ function randomizePosition() {
         randomY = Math.floor(Math.random() * maxY);
     } while (
         // Check for overlap with text box
-        (randomX >= textBox.offsetLeft - button.clientWidth &&
-        randomX <= textBox.offsetLeft + textBox.clientWidth &&
-        randomY >= textBox.offsetTop &&
-        randomY <= textBox.offsetTop + textBox.clientHeight) ||
+        (randomX >= textBox.offsetLeft - 5 &&
+        randomX <= textBox.offsetLeft + textBox.clientWidth + 5 &&
+        randomY >= textBox.offsetTop - 5 &&
+        randomY <= textBox.offsetTop + textBox.clientHeight + 5) ||
         // Check for overlap with "Yes" button
-        (randomX >= yesButton.offsetLeft - button.clientWidth &&
-        randomX <= yesButton.offsetLeft + yesButton.clientWidth &&
-        randomY >= yesButton.offsetTop &&
-        randomY <= yesButton.offsetTop + yesButton.clientHeight)
+        (randomX >= yesButton.offsetLeft - 5 &&
+        randomX <= yesButton.offsetLeft + yesButton.clientWidth + 5 &&
+        randomY >= yesButton.offsetTop - 5 &&
+        randomY <= yesButton.offsetTop + yesButton.clientHeight + 5)
     );
+    
 
     button.style.position = 'absolute';
     button.style.left = randomX + 'px';
