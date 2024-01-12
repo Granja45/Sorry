a823b494d007ce0145614272f99635c082a39142
Granja45 <140736732+Granja45@users.noreply.github.com>
2024-01-12 00:54:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function randomizePosition() {
     var button = document.getElementById('noBtn');
     var textBox = document.querySelector('.container');
     var yesButton = document.getElementById('yesBtn');
-
+    
     var maxX = window.innerWidth - button.clientWidth;
     var maxY = window.innerHeight - button.clientHeight;
 
@@ -11,25 +11,50 @@ function randomizePosition() {
     do {
         randomX = Math.floor(Math.random() * maxX);
         randomY = Math.floor(Math.random() * maxY);
+
+        // Adjust position if it exceeds maximum width
+        if (randomX + button.clientWidth > maxX) {
+            randomX = maxX - button.clientWidth;
+        }
+
+        // Adjust position if it exceeds maximum height
+        if (randomY + button.clientHeight > maxY) {
+            randomY = maxY - button.clientHeight;
+        }
+
+        // Ensure at least 20 pixels away from the text box on all sides
+        var minDistance = 20;
+
+        if (
+            randomX + button.clientWidth + minDistance > textBox.offsetLeft &&
+            randomX - minDistance < textBox.offsetLeft + textBox.clientWidth &&
+            randomY + button.clientHeight + minDistance > textBox.offsetTop &&
+            randomY - minDistance < textBox.offsetTop + textBox.clientHeight
+        ) {
+            randomX = textBox.offsetLeft + textBox.clientWidth + minDistance;
+            randomY = textBox.offsetTop + minDistance;
+        }
+
+        console.log('randomX', randomX);
+        console.log('randomY', randomY);
+
     } while (
         // Check for overlap with text box
-        (randomX >= textBox.offsetLeft - 20 &&
-        randomX <= textBox.offsetLeft + textBox.clientWidth + 20 &&
-        randomY >= textBox.offsetTop - 20 &&
-        randomY <= textBox.offsetTop + textBox.clientHeight + 20) ||
+        (randomX >= textBox.offsetLeft - minDistance &&
+        randomX <= textBox.offsetLeft + textBox.clientWidth + minDistance &&
+        randomY >= textBox.offsetTop - minDistance &&
+        randomY <= textBox.offsetTop + textBox.clientHeight + minDistance) ||
         // Check for overlap with "Yes" button
-        (randomX >= yesButton.offsetLeft - 20 &&
-        randomX <= yesButton.offsetLeft + yesButton.clientWidth + 20 &&
-        randomY >= yesButton.offsetTop - 20 &&
-        randomY <= yesButton.offsetTop + yesButton.clientHeight + 20)
+        (randomX >= yesButton.offsetLeft - minDistance &&
+        randomX <= yesButton.offsetLeft + yesButton.clientWidth + minDistance &&
+        randomY >= yesButton.offsetTop - minDistance &&
+        randomY <= yesButton.offsetTop + yesButton.clientHeight + minDistance)
     );
-    
 
     button.style.position = 'absolute';
     button.style.left = randomX + 'px';
     button.style.top = randomY + 'px';
 }
-
 
 function redirectToLove() {
     window.location.href = 'love.html';
